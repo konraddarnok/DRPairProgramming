1a4825d8db6540a7b40dca0f2c7beb3c9cc4e51f
Tali0139 <47971571+Tali0139@users.noreply.github.com>
2020-03-24 10:05:54 +0100

    lavet getter

--- DRPairProgramming/Models/MusicRecord.cs
+++ DRPairProgramming/Models/MusicRecord.cs
@@ -5,31 +5,61 @@ using System.Threading.Tasks;
 
 namespace DRPairProgramming.Models
 {
-    public class MusicRecord : Artist
+    public class MusicRecord
     {
-        
-        private string _recordTitle;    
+
+        private string _recordTitle;
         private double _recordDuration;
-        private int _yearOfpoplication;
+        private int _yearOfPuplication;
         private Artist _artist;
-        
-        public MusicRecord(string recordTitle, Artist artist, double recordDuration, int yearOfpoplication) 
+        //private enum _genre
+        //{
+        //    jazz,
+        //    rock, 
+        //    pop,
+        //    alternative,
+        //    classic
+        //}
+
+
+
+        public MusicRecord(string recordTitle, Artist artist, double recordDuration, int yearOfPuplication)
         {
             RecordTitle = recordTitle;
+            Artist = artist;
+            recordDuration = recordDuration;
+
         }
+
         public string RecordTitle
         {
-            get
-            {
-                return _recordTitle;
-            }
-            set
-            {
-                _recordTitle = value;
-            }
+            get { return _recordTitle; }
+            set { _recordTitle = value; }
+        }
 
+
+        public Artist Artist
+        {
+            get { return _artist; }
+            set { _artist = value; }
+        }
+
+        public double RecordDuration
+        {
+            get { return _recordDuration; }
+            set { _recordDuration = value; }
+        }
+
+        public int YearOfPublication
+        {
+            get { return _yearOfPuplication; }
+            set { _yearOfPuplication = value; }
         }
         
-
     }
 }
+
+
+
+
+
