82395958e9baf55bc36e480d11b6b3f7d2e87e02
konrad <konrad@festinafinance.com>
2020-03-24 09:47:38 +0100

    Artist and MusicRecord Class add

---
+++ DRPairProgramming/Models/MusicRecord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DRPairProgramming.Models
+{
+    public class MusicRecord : Artist
+    {
+        
+        private string _recordTitle;    
+        private double _recordDuration;
+        private int _yearOfpoplication;
+        private Artist _artist;
+        
+        public MusicRecord(string recordTitle, Artist artist, double recordDuration, int yearOfpoplication) 
+        {
+            RecordTitle = recordTitle;
+        }
+        public string RecordTitle
+        {
+            get
+            {
+                return _recordTitle;
+            }
+            set
+            {
+                _recordTitle = value;
+            }
+
+        }
+        
+
+    }
+}
