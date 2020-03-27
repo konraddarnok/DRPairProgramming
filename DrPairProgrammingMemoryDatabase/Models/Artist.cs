153a29c24c4175402e315d093fc06a5c7e980451
Konrad Schantz Kirkebjerg <konr0693@edu.easj.dk>
2020-03-27 10:47:29 +0100

    cfommit

--- DrPairProgrammingMemoryDatabase/Models/Artist.cs
+++ DrPairProgrammingMemoryDatabase/Models/Artist.cs
@@ -10,12 +10,14 @@ namespace DrPairProgrammingMemoryDatabase.Models
         private string _artistName;
         private string _recordLabel;
         private string _country;
+        private int _id;
         
         public string ArtistName { get => _artistName; set => _artistName = value; }
         public string RecordLabel { get => _recordLabel; set => _recordLabel = value; }
         public string Country { get => _country; set => _country = value; }
+        public int Id { get => _id; set => _id = value; }
 
-        public Artist(string artistName, string recordLabel, string country)
+        public Artist(int id, string artistName, string recordLabel, string country)
         {
             ArtistName = artistName;
             RecordLabel = recordLabel;
