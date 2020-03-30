5c2be72518aad2f65c480837d7ff615383a9827c
Tali0139 <47971571+Tali0139@users.noreply.github.com>
2020-03-30 12:16:08 +0200

    Added post-test

--- DRPairProgrammingTests/Models/MusicRecordTests.cs
+++ DRPairProgrammingTests/Models/MusicRecordTests.cs
@@ -9,18 +9,20 @@ namespace DRPairProgramming.Models.Tests
 {
     [TestClass()]
     public class MusicRecordTests
-    { 
+    {
 
-        [TestMethod(),ExpectedException(typeof(ArgumentException))]
+        [TestMethod(), ExpectedException(typeof(ArgumentException))]
         public void TitleNullException()
         {
-            MusicRecord record1 = new MusicRecord("", new Artist("Madonna", "Sony", "America"), 25.45, 1999, MusicRecord._genre.pop);
+            MusicRecord record1 = new MusicRecord("", new Artist("Madonna", "Sony", "America"), 25.45, 1999,
+                MusicRecord._genre.pop);
         }
 
         [TestMethod()]
         public void MusicRecordTest1()
         {
-            MusicRecord record2 = new MusicRecord("MyMusic", new Artist("Madonna", "Sony", "America"), 25.45, 1999, MusicRecord._genre.pop);
+            MusicRecord record2 = new MusicRecord("MyMusic", new Artist("Madonna", "Sony", "America"), 25.45, 1999,
+                MusicRecord._genre.pop);
             Assert.AreEqual(record2.Artist.ArtistName, "Madonna");
             Assert.AreNotEqual(record2.YearOfPublication, 2001);
         }
@@ -29,20 +31,24 @@ namespace DRPairProgramming.Models.Tests
         public void TestGetMethods()
         {
             MusicRecordController controller = new MusicRecordController();
-           
+
             controller.Get();
-            
-            Assert.AreEqual(controller.MusicRecordsList.Count,3);
-            Assert.AreNotEqual(controller.MusicRecordsList.Count,2);
+
+            Assert.AreEqual(controller.MusicRecordsList.Count, 3);
+            Assert.AreNotEqual(controller.MusicRecordsList.Count, 2);
         }
 
         [TestMethod()]
         public void TestPostMethods()
         {
             MusicRecordController controller = new MusicRecordController();
+            int currentCount = controller.MusicRecordsList.Count;
+            int afterPost = currentCount + 1;
 
-            //SKal implementeres
+            controller.Post(new MusicRecord("MyMusicTest", new Artist("Eminem", "Empire Records", "USA"), 123.4, 2020,
+                MusicRecord._genre.jazz));
+            Assert.AreEqual(controller.MusicRecordsList.Count, afterPost);
+
         }
-
     }
 }
